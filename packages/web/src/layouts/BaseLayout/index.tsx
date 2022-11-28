64941cfb8e4fe2c911fb3c54f569fa0b040851d4
felon <1104377019@qq.com>
2022-11-28 15:15:16 +0800

    feat: layout

--- packages/web/src/layouts/BaseLayout/index.tsx
+++ packages/web/src/layouts/BaseLayout/index.tsx
@@ -1,66 +1,350 @@
-import React, { useState } from 'react';
 import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
+  CaretDownFilled,
+  DoubleRightOutlined,
+  GithubFilled,
+  InfoCircleFilled,
+  PlusCircleFilled,
+  QuestionCircleFilled,
+  SearchOutlined,
 } from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
-import styles from './index.module.scss'
+import ProCard from '@ant-design/pro-card';
+import type { ProSettings } from '@ant-design/pro-layout';
+import { PageContainer, ProLayout, SettingDrawer } from '@ant-design/pro-layout';
+import { ProProvider } from '@ant-design/pro-provider';
+import { css } from '@emotion/css';
+import { Button, Divider, Popover, Input, theme } from 'antd';
+import React, { useState } from 'react';
+import defaultProps from './_defaultProps';
+import {Outlet} from 'react-router-dom'
 
-const { Header, Sider, Content } = Layout;
-
-const App: React.FC = () => {
-  const [collapsed, setCollapsed] = useState(false);
-
+const Item: React.FC<{ children: React.ReactNode }> = (props) => {
+  const { token } = theme.useToken();
   return (
-    <Layout className={styles.container}>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className={styles.logo} />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
-      </Sider>
-      <Layout className={styles.siteLayout}>
-        <Header className={styles.siteLayoutBackground} style={{ padding: 0 }}>
-          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-            className: 'trigger',
-            onClick: () => setCollapsed(!collapsed),
-          })}
-        </Header>
-        <Content
-          className={styles.siteLayoutBackground}
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-          }}
-        >
-          Content
-        </Content>
-      </Layout>
-    </Layout>
+    <div
+      className={css`
+        color: ${token.colorTextSecondary};
+        font-size: 14px;
+        cursor: pointer;
+        line-height: 22px;
+        margin-bottom: 8px;
+        &:hover {
+          color: ${token.colorPrimary};
+        }
+      `}
+      style={{
+        width: '33.33%',
+      }}
+    >
+      {props.children}
+      <DoubleRightOutlined
+        style={{
+          marginInlineStart: 4,
+        }}
+      />
+    </div>
   );
 };
 
-export default App;
+const List: React.FC<{ title: string; style?: React.CSSProperties }> = (props) => {
+  const { token } = theme.useToken();
+
+  return (
+    <div
+      style={{
+        width: '100%',
+        ...props.style,
+      }}
+    >
+      <div
+        style={{
+          fontSize: 16,
+          color: token.colorTextHeading,
+          lineHeight: '24px',
+          fontWeight: 500,
+          marginBlockEnd: 16,
+        }}
+      >
+        {props.title}
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+        }}
+      >
+        {new Array(6).fill(1).map((_, index) => {
+          return <Item key={index}>具体的解决方案-{index}</Item>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+const MenuCard = () => {
+  const { token } = theme.useToken();
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      <Divider
+        style={{
+          height: '1.5em',
+        }}
+        type="vertical"
+      />
+      <Popover
+        placement="bottom"
+        overlayStyle={{
+          width: 'calc(100vw - 24px)',
+          padding: '24px',
+          paddingTop: 8,
+          height: '307px',
+          borderRadius: '0 0 6px 6px',
+        }}
+        content={
+          <div style={{ display: 'flex', padding: '32px 40px' }}>
+            <div style={{ flex: 1 }}>
+              <List title="金融解决方案" />
+              <List
+                title="其他解决方案"
+                style={{
+                  marginBlockStart: 32,
+                }}
+              />
+            </div>
+
+            <div
+              style={{
+                width: '308px',
+                borderInlineStart: '1px solid ' + token.colorBorder,
+                paddingInlineStart: 16,
+              }}
+            >
+              <div
+                className={css`
+                  font-size: 14px;
+                  color: ${token.colorText};
+                  line-height: 22px;
+                `}
+              >
+                热门产品
+              </div>
+              {new Array(3).fill(1).map((name, index) => {
+                return (
+                  <div
+                    key={index}
+                    className={css`
+                      border-radius: 4px;
+                      padding: 16px;
+                      margin-top: 4px;
+                      display: flex;
+                      cursor: pointer;
+                      &:hover {
+                        background-color: ${token.colorBgTextHover};
+                      }
+                    `}
+                  >
+                    <img src="https://gw.alipayobjects.com/zos/antfincdn/6FTGmLLmN/bianzu%25252013.svg" />
+                    <div
+                      style={{
+                        marginInlineStart: 14,
+                      }}
+                    >
+                      <div
+                        className={css`
+                          font-size: 14px;
+                          color: ${token.colorText};
+                          line-height: 22px;
+                        `}
+                      >
+                        Ant Design
+                      </div>
+                      <div
+                        className={css`
+                          font-size: 12px;
+                          color: ${token.colorTextSecondary};
+                          line-height: 20px;
+                        `}
+                      >
+                        杭州市较知名的 UI 设计语言
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        }
+      >
+        <div
+          style={{
+            color: token.colorTextHeading,
+            fontWeight: 500,
+            cursor: 'pointer',
+            display: 'flex',
+            gap: 4,
+            paddingInlineStart: 8,
+            paddingInlineEnd: 12,
+            alignItems: 'center',
+          }}
+          className={css`
+            &:hover {
+              background-color: ${token.colorBgTextHover};
+            }
+          `}
+        >
+          <span> 企业级资产中心</span>
+          <CaretDownFilled />
+        </div>
+      </Popover>
+    </div>
+  );
+};
+
+const SearchInput = () => {
+  const { token } = theme.useToken();
+  return (
+    <div
+      key="SearchOutlined"
+      aria-hidden
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        marginInlineEnd: 24,
+      }}
+      onMouseDown={(e) => {
+        e.stopPropagation();
+        e.preventDefault();
+      }}
+    >
+      <Input
+        style={{
+          borderRadius: 4,
+          marginInlineEnd: 12,
+          backgroundColor: token.colorBgTextHover,
+        }}
+        prefix={
+          <SearchOutlined
+            style={{
+              color: token.colorTextLightSolid,
+            }}
+          />
+        }
+        placeholder="搜索方案"
+        bordered={false}
+      />
+      <PlusCircleFilled
+        style={{
+          color: token.colorPrimary,
+          fontSize: 24,
+        }}
+      />
+    </div>
+  );
+};
+
+export default () => {
+  const [settings, setSetting] = useState<Partial<ProSettings> | undefined>({
+    fixSiderbar: true,
+    layout: 'mix',
+    splitMenus: true,
+  });
+
+  const [pathname, setPathname] = useState('/list/sub-page/sub-sub-page1');
+  const [num, setNum] = useState(40);
+  return (
+    <div
+      id="test-pro-layout"
+      style={{
+        height: '100vh',
+      }}
+    >
+      <ProProvider.Provider
+        value={{
+          hashed: false,
+        }}
+      >
+        <ProLayout
+          bgLayoutImgList={[
+            {
+              src: 'https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png',
+              left: 85,
+              bottom: 100,
+              height: '303px',
+            },
+            {
+              src: 'https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png',
+              bottom: -68,
+              right: -45,
+              height: '303px',
+            },
+            {
+              src: 'https://img.alicdn.com/imgextra/i3/O1CN018NxReL1shX85Yz6Cx_!!6000000005798-2-tps-884-496.png',
+              bottom: 0,
+              left: 0,
+              width: '331px',
+            },
+          ]}
+          {...defaultProps}
+          location={{
+            pathname,
+          }}
+          siderMenuType="group"
+          menu={{
+            collapsedShowGroupTitle: true,
+          }}
+          avatarProps={{
+            src: 'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
+            size: 'small',
+            title: '七妮妮',
+          }}
+          actionsRender={(props) => {
+            if (props.isMobile) return [];
+            return [
+              props.layout !== 'side' && document.body.clientWidth > 1400 ? (
+                <SearchInput />
+              ) : undefined,
+              <InfoCircleFilled key="InfoCircleFilled" />,
+              <QuestionCircleFilled key="QuestionCircleFilled" />,
+              <GithubFilled key="GithubFilled" />,
+            ];
+          }}
+          headerTitleRender={(logo, title, _) => {
+            const defaultDom = (
+              <a>
+                {logo}
+                {title}
+              </a>
+            );
+            if (document.body.clientWidth < 1400) {
+              return defaultDom;
+            }
+            if (_.isMobile) return defaultDom;
+            return (
+              <>
+                {defaultDom}
+                <MenuCard />
+              </>
+            );
+          }}
+          onMenuHeaderClick={(e) => console.log(e)}
+          menuItemRender={(item, dom) => (
+            <div
+              onClick={() => {
+                setPathname(item.path || '/welcome');
+              }}
+            >
+              {dom}
+            </div>
+          )}
+          {...settings}
+        >
+        <Outlet/>
+        </ProLayout>
+      </ProProvider.Provider>
+    </div>
+  );
+};
